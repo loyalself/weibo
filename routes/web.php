98c3c5962cf86961f7c7ce20ae01ab6912531402
loyalself <plafhtcc@163.com>
2019-11-22 07:10:31 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
+/**
+ * 路由后面加个name的意思:对应html页面,比如:name('abc') ,则在html页面链接应该href='abc'这样写找的到
+ */
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
