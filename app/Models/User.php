1513216d514c5799562f6b7178dfe9de2ef4c74f
loyalself <plafhtcc@163.com>
2019-11-25 01:06:55 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     * 允许被修改的字段
+     *
      * @var array
      */
     protected $fillable = [
@@ -41,4 +41,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 生成用户头像方法
+     * @param string $size
+     * @return string
+     */
+    public function gravatar($size = '100')
+    {
+        /**
+         * 通过 $this->attributes['email'] 获取到用户的邮箱
+         * 将小写的邮箱使用 md5 方法进行转码；
+         * 将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
+         */
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,22 @@
  * 路由后面加个name的意思:对应html页面,比如:name('abc') ,则在html页面链接应该href='abc'这样写找的到
  */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+
+/**
+ *  resource 方法将遵从 RESTful 架构为用户资源生成路由。该方法接收两个参数，第一个参数 为资源名称，第二个参数为控制器名称
+ */
+Route::resource('users', 'UsersController');
+//Route::get('/users', 'UsersController@index')->name('users.index');                显示所有用户列表的页面
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');           显示用户个人信息的页面
+//Route::get('/users/create', 'UsersController@create')->name('users.create');       创建用户的页面
+//Route::post('/users', 'UsersController@store')->name('users.store');               创建用户
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');      编辑用户个人资料的页面
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');     更新用户
+//Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');  删除用户
