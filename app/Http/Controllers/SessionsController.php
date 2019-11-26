08b5ea206e3e446c0251ed9e44699539b4553c6b
loyalself <plafhtcc@163.com>
2019-11-26 01:08:21 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,6 +11,12 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
+    /**
+     * 登录
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store(Request $request)
     {
         $credentials = $this->validate($request, [
@@ -27,5 +33,16 @@ class SessionsController extends Controller
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput(); }
+    }
+
+    /**
+     * 退出
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
