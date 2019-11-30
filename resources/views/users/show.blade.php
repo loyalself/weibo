32f6d7e7686983bed96d9e64fa0f9d3322753289
loyalself <plafhtcc@163.com>
2019-11-30 01:18:55 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,14 +3,18 @@
 @section('title', $user->name)
 
 @section('content')
-  <div class="row"> <div class="offset-md-2 col-md-8">
+  <div class="row">
+    <div class="offset-md-2 col-md-8">
       <section class="user_info">
         {{--
            我们可以通过给 @include 方法传参，将用户数据以关联数组的形式传送到 _user_info 局部视图 上。
            --}}
         @include('shared._user_info', ['user' => $user])
       </section>
-
+      <section class="stats mt-2">
+        @include('shared._stats', ['user' => $user])
+      </section>
+      <hr>
       <section class="status">
         @if ($statuses->count() > 0)
           <ul class="list-unstyled">
